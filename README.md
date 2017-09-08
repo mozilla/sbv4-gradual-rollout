135051585de815ea8b42d537afcb19b24265e26e
Francois Marier <francois@mozilla.com>
2017-09-08 16:24:57 -0700

    Initial working version of the add-on

---
+++ README.md
@@ -0,0 +1,16 @@
+# Safe Browsing V4 gradual roll-out
+
+<https://bugzilla.mozilla.org/show_bug.cgi?id=1393980>
+
+## Building the add-on
+
+    zip -r ../sbv4-gradual-rollout.xpi *
+
+## Installing the add-on in Firefox
+
+1. `about:config`, set
+   1. `extensions.legacy.enabled` to `true`
+   2. `xpinstall.signatures.required` to `false`
+2. `about:debugging > [load temporary addon] > choose `dist/addon.xpi`
+3. `tools > Web Developer > Browser Toolbox`
+
---
+++ bootstrap.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const {utils: Cu} = Components;
+Cu.import("resource://gre/modules/Services.jsm");
+const UserPreferences = Services.prefs;
+const DefaultPreferences = Services.prefs.getDefaultBranch("");
+
+function startup(data) {
+  DefaultPreferences.setStringPref("urlclassifier.malwareTable", "goog-malware-proto,goog-unwanted-proto,test-malware-simple,test-unwanted-simple");
+  DefaultPreferences.setStringPref("urlclassifier.phishTable", "goog-phish-proto,test-phish-simple");
+  DefaultPreferences.setStringPref("urlclassifier.downloadAllowTable", "goog-downloadwhite-proto");
+  DefaultPreferences.setStringPref("urlclassifier.downloadBlockTable", "goog-badbinurl-proto");
+
+  // forcing reinitialization re: https://github.com/raymak/sbv4-crash-shield-study/issues/4
+  UserPreferences.setStringPref("browser.safebrowsing.provider.google.advisoryName", "Google Safe Browsing");
+}
+
+function shutdown(data) {
+  // cleanup not required since the value of the UserPref we set is the new default in 57+
+}
