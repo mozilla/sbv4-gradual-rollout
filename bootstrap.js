2fd979bfb046b081f9521d90b5a1e05806177925
Francois Marier <francois@mozilla.com>
2017-09-12 16:46:39 -0700

    Make the add-on uninstall itself on 57
    Fx 57 will default to Safe Browsing V4 so we should ensure that we
    remove the add-on if it's not already done.

--- bootstrap.js
+++ bootstrap.js
@@ -2,10 +2,17 @@
 
 const {utils: Cu} = Components;
 Cu.import("resource://gre/modules/Services.jsm");
+Cu.import("resource://gre/modules/AddonManager.jsm");
 const UserPreferences = Services.prefs;
 const DefaultPreferences = Services.prefs.getDefaultBranch("");
 
 function startup(data) {
+  if (parseInt(Services.appinfo.version) >= 57) {
+    console.log("SBv4-gradual-rollout not needed on 57 or later, removing.");
+    AddonManager.getAddonByID("sbv4-gradual-rollout@mozilla.com", addon => addon.uninstall());
+    return;
+  }
+
   // Switch default to SBv4
   DefaultPreferences.setStringPref("urlclassifier.malwareTable", "goog-malware-proto,goog-unwanted-proto,test-malware-simple,test-unwanted-simple");
   DefaultPreferences.setStringPref("urlclassifier.phishTable", "goog-phish-proto,test-phish-simple");
@@ -17,6 +24,11 @@ function startup(data) {
 }
 
 function shutdown(data) {
+  if (parseInt(Services.appinfo.version) >= 57) {
+    console.log("No cleanup necessary on 57 or later.");
+    return;
+  }
+
   // Set default back to SBv2
   DefaultPreferences.setStringPref("urlclassifier.malwareTable", "goog-malware-shavar,goog-unwanted-shavar,test-malware-simple,test-unwanted-simple");
   DefaultPreferences.setStringPref("urlclassifier.phishTable", "goog-phish-shavar,test-phish-simple");
